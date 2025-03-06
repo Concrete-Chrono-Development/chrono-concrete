Concrete-Chrono-Development/chrono-concrete

commit 7ef5d38b5760d3f95f5311d38a1c6b861233b3e0

rename variables for clarity. Replace ChVectorDynamic by ChVectorN of the same size when size is known at compile time and ChVectorDynamic type is not inherited from parent

src/chrono_wood/ChElementCBLCON.cpp
@@ -25,6 +25,7 @@
 
 #include "chrono_wood/ChElementCBLCON.h"
 #include <iomanip>
+#include "chrono/core/ChVector3.h"
 
 namespace chrono {
 namespace wood {
@@ -123,7 +124,7 @@ void ChElementCBLCON::Update() {
 
     // always keep updated the rotation matrix A:
 	if (ChElementCBLCON::LargeDeflection){
-		this->UpdateRotation();    
+		this->UpdateRotation();
 		this->section->Set_Length((nodes[1]->GetPos() - nodes[0]->GetPos()).Length());
 	}	
 	//
@@ -134,8 +135,8 @@ void ChElementCBLCON::Update() {
     // update total displacement increment 
     //ChVectorDynamic<> displ(12);
     //this->GetStateBlock(displ);
-    //DUn_1=displ-Un_1;
-    //Un_1=displ;  
+    //dofs_increment=displ-dofs_old;
+    //dofs_old=displ;
     //
     // Compute local stiffness matrix:
     //
@@ -222,15 +223,6 @@ void ChElementCBLCON::GetStateBlock(ChVectorDynamic<>& mD) {
 }
 
 
-
-void ChElementCBLCON::GetLatticeStateBlock(ChVectorDynamic<>& mD) {
-    mD.setZero(12);
-    mD.segment(0, 6)=DUn_1.segment(0,6); 
-    mD.segment(6, 6)=DUn_1.segment(6,6); 
-}
-
-
-
 void ChElementCBLCON::GetField_dt(ChVectorDynamic<>& mD_dt) {
     mD_dt.resize(12);
 
@@ -248,8 +240,7 @@ void ChElementCBLCON::GetField_dt(ChVectorDynamic<>& mD_dt) {
 }
 
 
-void ChElementCBLCON::ComputeStrain(ChVectorDynamic<>& displ_incr, ChVectorDynamic<>& mstrain, ChVectorDynamic<>& curvature) {
-    mstrain.resize(3);
+void ChElementCBLCON::ComputeStrainIncrement(ChVectorN<double, 12>& displ_incr, ChVector3d& mstrain, ChVector3d& curvature) {
 	// 
 	auto dispN1=displ_incr.segment(0,6);
 	auto dispN2=displ_incr.segment(6,6);
@@ -293,7 +284,7 @@ void ChElementCBLCON::ComputeStrain(ChVectorDynamic<>& displ_incr, ChVectorDynam
 			ChMatrixNM<double,1,6> BJ= n.transpose()*AJ/length;			
 			//std::cout << "matrix BJ:\n" << BJ << std::endl; 
 			//
-			mstrain(id)=double (BJ*dispN2)+ double (-BI*dispN1); 
+			mstrain[id]=double (BJ*dispN2)+ double (-BI*dispN1);
 
 		}
 		
@@ -309,29 +300,25 @@ void ChElementCBLCON::ComputeStrain(ChVectorDynamic<>& displ_incr, ChVectorDynam
 
 
 
-void ChElementCBLCON::ComputeStress(ChVectorDynamic<>& mstress) {
-    mstress.resize(3);
-	ChVectorDynamic<> mstrain;
-	ChVectorDynamic<> curvature;
-    // Displacement increment of nodes:
-	ChVectorDynamic<> displ_incr(this->GetNumCoordsPosLevel());
-	this->GetLatticeStateBlock(displ_incr);
-	this->ComputeStrain(displ_incr, mstrain, curvature);
+void ChElementCBLCON::ComputeStress(ChVector3d& mstress) {
+	ChVector3d mstrain;
+	ChVector3d curvature;
+    // Displacement and rotation increment of nodes:
+	this->ComputeStrainIncrement(dofs_increment, mstrain, curvature);
 	//std::cout<<"\nmstrain:\n"<<mstrain<<std::endl;	
 	//
 	double E0=this->section-> Get_material()->Get_E0();
 	double alpha=this->section-> Get_material()->Get_alpha();	
 	//
-	double epsQ=pow(mstrain(0)*mstrain(0)+alpha*(mstrain(1)*mstrain(1)+mstrain(2)*mstrain(2)), 0.5);
+	double epsQ=pow(mstrain[0]*mstrain[0]+alpha*(mstrain[1]*mstrain[1]+mstrain[2]*mstrain[2]), 0.5);
 	double strsQ=E0*epsQ;	
 	//
 	if (epsQ!=0) {
-		mstress(0)=strsQ*mstrain(0)/epsQ;
-		mstress(1)=alpha*strsQ*mstrain(1)/epsQ;
-		mstress(2)=alpha*strsQ*mstrain(2)/epsQ;
+		mstress[0]=strsQ*mstrain[0]/epsQ;
+		mstress[1]=alpha*strsQ*mstrain[1]/epsQ;
+		mstress[2]=alpha*strsQ*mstrain[2]/epsQ;
 	}else{
-		mstress<< 0.0, 0.0, 0.0;
-		//mstress.setZero();
+		mstress.Set(0.0);
 	}	
 }
 
@@ -541,21 +528,19 @@ void ChElementCBLCON::SetupInitial(ChSystem* system) {
     //
     ChVectorDynamic<> displ(12);
     this->GetStateBlock(displ);
-    Un_1=displ;
+    dofs_old=displ;
     //
     // Initiliaze state variables:
     //
-	
-    auto statev= this->section->Get_StateVar();
-    statev.resize(18);
-    statev.setZero();
-    this->section->Set_StateVar(statev); 
+	// TODO: JBC, this should arguably be set in the section's constructor initialization list ?
+    this->section->Get_StateVar().setZero();
+
     //
 	if(this->macro_strain){
 			this->section->ComputeProjectionMatrix();
 			this->section->ComputeEigenStrain(this->macro_strain);			
 	}
-    	
+
     //
     // Compute local stiffness matrix:
     //
@@ -573,7 +558,6 @@ void ChElementCBLCON::ComputeKRMmatricesGlobal(ChMatrixRef H, double Kfactor, do
     //
     // The K stiffness matrix and R damping matrix of this element:
     //
-
     if (Kfactor || Rfactor) {    
 
         if (use_numerical_diff_for_KR) {
@@ -596,7 +580,7 @@ void ChElementCBLCON::ComputeKRMmatricesGlobal(ChMatrixRef H, double Kfactor, do
                 ChVector3d paD = pa0; 
                 paD[i] += delta_p;
                 this->GetNodeA()->SetPos(paD);
-                this->ComputeInternalForces(FiD);                
+                this->ComputeInternalForces(FiD);              
                 H_num.block<12,1>(0, i) = (FiD - Fi0) / delta_p;
                 this->GetNodeA()->SetPos(pa0);
             }
@@ -763,13 +747,12 @@ void ChElementCBLCON::ComputeInternalForces(ChVectorDynamic<>& Fi) {
     assert(Fi.size() == 12);
     assert(section);
     
+    // Displacement and rotation increment of nodes:
     ChVectorDynamic<> displ(12);
     this->GetStateBlock(displ);
-    DUn_1=displ-Un_1;
-    Un_1=displ;	
-    // Displacement increment of nodes:
-	ChVectorDynamic<> displ_incr(this->GetNumCoordsPosLevel());
-	this->GetLatticeStateBlock(displ_incr);
+    dofs_increment = displ - dofs_old;
+    dofs_old = displ;
+
 	//
 	// Get facet area and length of beam
 	//
@@ -821,12 +804,12 @@ void ChElementCBLCON::ComputeInternalForces(ChVectorDynamic<>& Fi) {
 	//this->ComputeStress(mstress);
 	
 	auto mysection=this->section;
-	ChVectorDynamic<> mstress;	
-	ChVectorDynamic<> dmstrain;
-	ChVectorDynamic<> mcouple;
-	ChVectorDynamic<> dcurvature;
-	ChVectorDynamic<> statev;
-	this->ComputeStrain(displ_incr, dmstrain, dcurvature);
+	ChVector3d mstress;
+	ChVector3d dmstrain;
+	ChVector3d mcouple;
+	ChVector3d dcurvature;
+	StateVarVector statev;
+	this->ComputeStrainIncrement(dofs_increment, dmstrain, dcurvature);
 	statev=mysection->Get_StateVar();
 	// compute volumetric strain
 	double epsV=0;
@@ -855,8 +838,8 @@ void ChElementCBLCON::ComputeInternalForces(ChVectorDynamic<>& Fi) {
     // [local Internal Forces] = [Klocal] * displ + [Rlocal] * displ_dt
     ChVectorDynamic<> Fi_local(12);
 	
-	Fi_local.segment(0,6)=-area*length*(mstress.transpose()*mBI);
-	Fi_local.segment(6,6)= area*length*(mstress.transpose()*mBJ);	
+	Fi_local.segment(0,6)=-area*length*(mstress.eigen().transpose()*mBI);
+	Fi_local.segment(6,6)= area*length*(mstress.eigen().transpose()*mBJ);
 	
 	//std::cout<<"\nFi_local-1:\n"<<Fi_local<<std::endl;
 	ChVectorDynamic<> Fmu(6);
@@ -869,7 +852,7 @@ void ChElementCBLCON::ComputeInternalForces(ChVectorDynamic<>& Fi) {
 		Bx.block<3,3>(0,0)=-Bx.block<3,3>(3,0);
 		
 		//ChVectorDynamic<> Fmu(6);
-		Fmu=area*(Bx*mcouple);
+		Fmu=area*(Bx*mcouple.eigen());
 		//std::cout<<"length:\t"<<length<<"area:\t"<<area<<std::endl;
 		//std::cout<<"mcouple:\t"<<mcouple<<std::endl;
 		//std::cout<<"nmL:\n"<<nmL<<std::endl;	

src/chrono_wood/ChBeamSectionCBLCON.h
@@ -29,6 +29,7 @@
 #include <memory>
 #include <string>
 
+#include "chrono/core/ChMatrix.h"
 #include "chrono_wood/ChWoodApi.h"
 #include "chrono_wood/ChWoodMaterialVECT.h"
 #include "chrono/core/ChMatrix33.h"
@@ -48,6 +49,9 @@ namespace wood {
 //class ChWoodApi ChBeamSectionCBLCON : public ChBeamSection {
 class ChWoodApi ChBeamSectionCBLCON  : public ChBeamSection { 
   public:
+
+    using StateVarVector = ChVectorN<double, 18>;
+
     ChBeamSectionCBLCON(  std::shared_ptr<ChWoodMaterialVECT> material,  // material 
                                     double area,    // Projected total area of the facet
                                     ChVector3d center,    // Center point of the facet area      
@@ -88,8 +92,8 @@ class ChWoodApi ChBeamSectionCBLCON  : public ChBeamSection {
     ChMatrix33<double> Get_facetFrame() const { return m_facetFrame; }
     void Set_facetFrame( ChMatrix33<double> facetFrame) { m_facetFrame=facetFrame; }
   
-    ChVectorDynamic<>  Get_StateVar() const { return m_state; };
-    void Set_StateVar(ChVectorDynamic<>  state) { m_state=state; }  
+    StateVarVector  Get_StateVar() const { return m_state; };
+    void Set_StateVar(StateVarVector  state) { m_state=state; }
     
     enum ConSectionType { transverse_generic, transverse_top, transverse_bot, longitudinal };
     
@@ -123,7 +127,7 @@ class ChWoodApi ChBeamSectionCBLCON  : public ChBeamSection {
     std::shared_ptr<ChWoodMaterialVECT> m_material;
     ChVector3d m_center;
     ChMatrix33<double> m_facetFrame;
-    ChVectorDynamic<>  m_state; 
+    StateVarVector m_state;
 	ChVectorDynamic<>  m_nonMechanicStrain;
     //std::shared_ptr<ChInternalDataCSL> m_state;  
     double mcon_width=1.0;  

src/chrono_wood/ChElementCBLCON.h
@@ -47,6 +47,7 @@ class ChWoodApi ChElementCBLCON : public ChElementBeam,
   public:
     using ShapeVector = ChMatrixNM<double, 1, 10>;
     using Amatrix = ChMatrixNM<double,3,6> ;
+    using StateVarVector = ChVectorN<double, 18>;
 
     ChElementCBLCON();
 
@@ -158,8 +159,6 @@ class ChWoodApi ChElementCBLCON : public ChElementBeam,
     /// Given that this element includes rotations at nodes, this gives:
     ///  {x_a y_a z_a Rx_a Ry_a Rz_a x_b y_b z_b Rx_b Ry_b Rz_b}
     virtual void GetStateBlock(ChVectorDynamic<>& mD) override;
-    
-    void GetLatticeStateBlock(ChVectorDynamic<>& mD);
 
     /// Fills the Ddt vector with the current time derivatives of field values at the nodes of the element, with proper ordering.
     /// If the D vector has not the size of this->GetNdofs(), it will be resized.
@@ -172,12 +171,12 @@ class ChWoodApi ChElementCBLCON : public ChElementBeam,
 	///
 	/// Compute strain at a CBLCON facet 
 	///
-	void ComputeStrain(ChVectorDynamic<>& displ_incr, ChVectorDynamic<>& mStrain, ChVectorDynamic<>& curvature);
+	void ComputeStrainIncrement(ChVectorN<double, 12>& displ_incr, ChVector3d& mStrain, ChVector3d& curvature);
 	///
 	///
-	/// Compute stress at a CBLCON facet 
+	/// Compute stress at a CBLCON facet . TODO : seems unused
 	///
-	void ComputeStress(ChVectorDynamic<>& mstress);
+	void ComputeStress(ChVector3d& mstress);
 	///
 	///	
 	virtual void ComputeMmatrixGlobal(ChMatrixRef M) override;
@@ -358,8 +357,8 @@ class ChWoodApi ChElementCBLCON : public ChElementBeam,
     ChQuaternion<> q_element_abs_rot;
     ChQuaternion<> q_element_ref_rot;
     
-    ChVectorDynamic<> DUn_1;
-    ChVectorDynamic<> Un_1;
+    ChVectorN<double, 12> dofs_increment;
+    ChVectorN<double, 12> dofs_old;
 
     bool disable_corotate;
     bool force_symmetric_stiffness;

src/chrono_wood/ChWoodMaterialVECT.h
@@ -20,6 +20,7 @@
 #ifndef CHWOODMATERIALVECT_H
 #define CHWOODMATERIALVECT_H
 
+#include "chrono/core/ChVector3.h"
 #include "chrono_wood/ChWoodApi.h"
 #include "chrono/core/ChMatrix33.h"
 #include <vector>
@@ -35,6 +36,8 @@ namespace wood {
 /// Definition of materials to be used for CSL beams and LDPM tets utilizing the lattice discrete particle model.
 class ChWoodApi ChWoodMaterialVECT {
   public:
+    using StateVarVector = ChVectorN<double, 18>;
+
     /// Construct an isotropic elastic material.
     ChWoodMaterialVECT(double rho,  		///< material density
                        double E0,    	///< Mesoscale Young's modulus
@@ -163,18 +166,18 @@ class ChWoodApi ChWoodMaterialVECT {
     double GetCoupleContrib2EquStrainFlag() const { return m_CoupleContrib2EquStrain; }
     void SetCoupleContrib2EquStrainFlag(double CoupleContrib2EquStrain) { m_CoupleContrib2EquStrain = CoupleContrib2EquStrain; }	
     /// Compute stresses from given strains and state variables.
-    void ComputeStress(ChVectorDynamic<>& mstrain, ChVectorDynamic<>& curvature, double &len, double& epsV, ChVectorDynamic<>& statev, double& area, double& width, double& height, ChVectorDynamic<>& mstress, ChVectorDynamic<>& mcouple);
+    void ComputeStress(ChVector3d& mstrain, ChVector3d& curvature, double &len, double& epsV, StateVarVector& statev, double& area, double& width, double& height, ChVector3d& mstress, ChVector3d& mcouple);
     //
-	void ComputeStress(ChVectorDynamic<>& mstrain, ChVectorDynamic<>& curvature, ChVectorDynamic<>& eigenstrain, double &len, double& epsV, ChVectorDynamic<>& statev, double& area, double& width, double& height, ChVectorDynamic<>& mstress, ChVectorDynamic<>& mcouple);
+	void ComputeStress(ChVector3d& mstrain, ChVector3d& curvature, ChVectorDynamic<>& eigenstrain, double &len, double& epsV, StateVarVector& statev, double& area, double& width, double& height, ChVector3d& mstress, ChVector3d& mcouple);
     
-    double FractureBC(ChVectorDynamic<>& mstrain, double& len, double& epsQ, double& epsT, ChVectorDynamic<>& statev);
+    double FractureBC(ChVector3d& mstrain, double& len, double& epsQ, double& epsT, StateVarVector& statev);
 
     //double CompressBC(ChVectorDynamic<>& mstrain, double& epsV, ChVectorDynamic<>& statev);
-	double CompressBC(ChVectorDynamic<>& mstrain, double& len, double& epsQ, double& epsT, ChVectorDynamic<>& statev);
+	double CompressBC(ChVector3d& mstrain, double& len, double& epsQ, double& epsT, StateVarVector& statev);
 
-    std::pair<double, double> ShearBC(ChVectorDynamic<>& mstrain, ChVectorDynamic<>& statev);
+    std::pair<double, double> ShearBC(ChVector3d& mstrain, StateVarVector& statev);
 	
-	std::vector<double> ShearBC(ChVectorDynamic<>& mstrain, ChVectorDynamic<>& curvature, double rN2, ChVectorDynamic<>& statev);
+	std::vector<double> ShearBC(ChVector3d& mstrain, ChVector3d& curvature, double rN2, StateVarVector& statev);
     
   private:
     

src/tests/unit_tests/wood/utest_WOOD_CBLCON.cpp
@@ -100,64 +100,64 @@ TEST(CBLConnectorTest, compute_strain){
 
     double disp = 0.01;
     double rot = 0.01;
-    ChVectorDynamic<> strain;
-    ChVectorDynamic<> curvature;
-    ChVectorDynamic<> displ_incr;
+    ChVector3d strain;
+    ChVector3d curvature;
+    ChVectorN<double, 12> displ_incr;
     displ_incr.setZero(12);
 
     // Translation of node A
     for (int i = 0; i<3 ; i++) {
         displ_incr(i) = disp; // X, Y, Z direction
-        connector->ComputeStrain(displ_incr, strain, curvature);
-        ASSERT_DOUBLE_EQ(strain(i), -disp / length);
-        ASSERT_DOUBLE_EQ(strain((i+1)%3), 0.0);
-        ASSERT_DOUBLE_EQ(strain((i+2)%3), 0.0);
+        connector->ComputeStrainIncrement(displ_incr, strain, curvature);
+        ASSERT_DOUBLE_EQ(strain[i], -disp / length);
+        ASSERT_DOUBLE_EQ(strain[(i+1)%3], 0.0);
+        ASSERT_DOUBLE_EQ(strain[(i+2)%3], 0.0);
         displ_incr(i) = 0.0;
     }
         // XYZ direction
         displ_incr(0) = displ_incr(1) = displ_incr(2) = disp;
-        connector->ComputeStrain(displ_incr, strain, curvature);
-        ASSERT_DOUBLE_EQ(strain(0), -disp / length);
-        ASSERT_DOUBLE_EQ(strain(1), -disp / length);
-        ASSERT_DOUBLE_EQ(strain(2), -disp / length);
+        connector->ComputeStrainIncrement(displ_incr, strain, curvature);
+        ASSERT_DOUBLE_EQ(strain[0], -disp / length);
+        ASSERT_DOUBLE_EQ(strain[1], -disp / length);
+        ASSERT_DOUBLE_EQ(strain[2], -disp / length);
         displ_incr(0) = displ_incr(1) = displ_incr(2) = 0.0;
 
     // Rotation of node A
     double dir[3][3] = {{0, 0, 0}, {0, 0, 1}, {0, -1, 0}}; // Direction of vector product: rot x (x_A - X_Center)
     for (int i = 0; i<3 ; i++) {
         displ_incr(3+i) = rot; // X, Y, Z direction
-        connector->ComputeStrain(displ_incr, strain, curvature);
-        ASSERT_DOUBLE_EQ(strain(0), rot * center_from_A * dir[i][0]);
-        ASSERT_DOUBLE_EQ(strain(1), rot * center_from_A * dir[i][1]);
-        ASSERT_DOUBLE_EQ(strain(2), rot * center_from_A * dir[i][2]);
+        connector->ComputeStrainIncrement(displ_incr, strain, curvature);
+        ASSERT_DOUBLE_EQ(strain[0], rot * center_from_A * dir[i][0]);
+        ASSERT_DOUBLE_EQ(strain[1], rot * center_from_A * dir[i][1]);
+        ASSERT_DOUBLE_EQ(strain[2], rot * center_from_A * dir[i][2]);
         displ_incr(3+i) = 0.0;
     }
 
     // Translation of node B
     for (int i = 0; i<3 ; i++) {
         displ_incr(6+i) = disp; // X, Y, Z direction
-        connector->ComputeStrain(displ_incr, strain, curvature);
-        ASSERT_DOUBLE_EQ(strain(i), disp / length);
-        ASSERT_DOUBLE_EQ(strain((i+1)%3), 0.0);
-        ASSERT_DOUBLE_EQ(strain((i+2)%3), 0.0);
+        connector->ComputeStrainIncrement(displ_incr, strain, curvature);
+        ASSERT_DOUBLE_EQ(strain[i], disp / length);
+        ASSERT_DOUBLE_EQ(strain[(i+1)%3], 0.0);
+        ASSERT_DOUBLE_EQ(strain[(i+2)%3], 0.0);
         displ_incr(6+i) = 0.0;
     }
         // XYZ direction
         displ_incr(6) = displ_incr(7) = displ_incr(8) = disp;
-        connector->ComputeStrain(displ_incr, strain, curvature);
-        ASSERT_DOUBLE_EQ(strain(0), disp / length);
-        ASSERT_DOUBLE_EQ(strain(1), disp / length);
-        ASSERT_DOUBLE_EQ(strain(2), disp / length);
+        connector->ComputeStrainIncrement(displ_incr, strain, curvature);
+        ASSERT_DOUBLE_EQ(strain[0], disp / length);
+        ASSERT_DOUBLE_EQ(strain[1], disp / length);
+        ASSERT_DOUBLE_EQ(strain[2], disp / length);
         displ_incr(6) = displ_incr(7) = displ_incr(8) = 0.0;
 
     // Rotation of node B
     // Direction of vector product: rot x (x_B - X_Center) is the same as for node A
     for (int i = 0; i<3 ; i++) {
         displ_incr(9+i) = rot; // X, Y, Z direction
-        connector->ComputeStrain(displ_incr, strain, curvature);
-        ASSERT_DOUBLE_EQ(strain(0), rot * (1 - center_from_A) * dir[i][0]);
-        ASSERT_DOUBLE_EQ(strain(1), rot * (1 - center_from_A) * dir[i][1]);
-        ASSERT_DOUBLE_EQ(strain(2), rot * (1 - center_from_A) * dir[i][2]);
+        connector->ComputeStrainIncrement(displ_incr, strain, curvature);
+        ASSERT_DOUBLE_EQ(strain[0], rot * (1 - center_from_A) * dir[i][0]);
+        ASSERT_DOUBLE_EQ(strain[1], rot * (1 - center_from_A) * dir[i][1]);
+        ASSERT_DOUBLE_EQ(strain[2], rot * (1 - center_from_A) * dir[i][2]);
         displ_incr(9+i) = 0.0;
     }
 
@@ -165,4 +165,8 @@ TEST(CBLConnectorTest, compute_strain){
     // ASSERT_DOUBLE_EQ(mstrain(0), 0.0);
     // ASSERT_DOUBLE_EQ(mstrain(1), 0.0);
     // ASSERT_DOUBLE_EQ(mstrain(2), 0.0);
+}
+
+TEST(CBLConnectorTest, update_rotation){
+    // TODO when we implement large displacement
 }

commit ac3bfa721c4957d5c9182ed68aae1ea5cf6950d0

fix bug I introduced in 7ef5d38b5760d3f95f5311d38a1c6b861233b3e0 by initializing state variables inside Section constructor.
section->Get_StateVar() does not return a reference and state variables were uninitialized.
Returning copies might be expensive, in the future consider making getters return references

src/chrono_wood/ChBeamSectionCBLCON.cpp
@@ -34,10 +34,12 @@ ChBeamSectionCBLCON::ChBeamSectionCBLCON( std::shared_ptr<ChWoodMaterialVECT> ma
                                     ChMatrix33<double> facetFrame    /// local system of frame of facet 
                                        )
     : m_material{material}, m_area{area}, m_center{center}, m_facetFrame{facetFrame} {
-        
+        m_state.setZero();
 }
 
-ChBeamSectionCBLCON::ChBeamSectionCBLCON() {};
+ChBeamSectionCBLCON::ChBeamSectionCBLCON() {
+	m_state.setZero();
+};
 
 ChBeamSectionCBLCON::~ChBeamSectionCBLCON() {};
 

src/chrono_wood/ChElementCBLCON.cpp
@@ -484,8 +484,8 @@ void ChElementCBLCON::SetupInitial(ChSystem* system) {
     //
     // Initiliaze state variables:
     //
-	// TODO: JBC, this should arguably be set in the section's constructor initialization list ?
-    this->section->Get_StateVar().setZero();
+	// All state variables initialized at zero, which is the value in the Section constructor
+    // Nothing to do here
 
     //
 	if(this->macro_strain){